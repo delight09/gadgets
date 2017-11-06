15923eb2180b68b626e335483fbd59afcb8c39c8
Jiahao Dai <hacka@djh.im>
2017-11-06 23:15:52 +0800

    Fix syntax error

--- file/placeholder_freshness_checker.sh
+++ file/placeholder_freshness_checker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash --
-# Delete files in placeholder, when lives longer than MINS_FRESH minutes.
+# Delete files in path when live longer than MINS_FRESH.
 # Files elder than MAGIC_MINS_ELDER_SAFTY won't get influenced.
 # NOTICE: MINS_FRESH should smaller than MAGIC_MINS_ELDER_SAFTY,
 # script should get triggered more than one time within MINS_FRESH.
@@ -9,4 +9,4 @@ MINS_FRESH=$1
 STR_PATH_PLACEHOLDER=$2
 MAGIC_MINS_ELDER_SAFTY=120
 
-echo find $STR_PATH_PLACEHOLDER -mmin +$MINS_FRESH -mmin -$MAGIC_MINS_ELDER_SAFTY -exec 'rm -f {} \;'
+find $STR_PATH_PLACEHOLDER -type f -mmin +$MINS_FRESH -mmin -$MAGIC_MINS_ELDER_SAFTY -exec rm -f {} \;
