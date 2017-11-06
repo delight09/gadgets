df41bdbd0e0940f52c316d4f6b9ad3758317c617
Jiahao Dai <hacka@djh.im>
2017-11-06 23:16:40 +0800

    Fullfill helper function

--- file/grive_phfc_helper.sh
+++ file/grive_phfc_helper.sh
@@ -1,10 +1,12 @@
-#!/bin/babsh --
-#
-# USAGE: grive_phfc_helper.sh
+#!/bin/bash --
+# Fire PHFC to clean files and use grive to upload(sync) changes
+# USAGE: grive_phfc_helper.sh 60 /srv/google-drive/Placeholder
 
+MINS_FRESH=$1
+STR_PATH_PLACEHOLDER=$2
 STR_DIR_GOOGLE_DRIVE_TARGET=Placeholder
-STR_PATH_PLACEHOLDER=/srv/google-drive
+STR_PATH_SYNCDIR=/srv/google-drive
 STR_URI_PHFC=/usr/local/bin/placeholder_freshness_checker.sh
 
-$STR_URI_PHFC # Control directory freshness
-grive -u -s $STR_DIR_GOOGLE_DRIVE -p $STR_PATH_PLACEHOLDER # upload changes by grive2
+$STR_URI_PHFC $MINS_FRESH $STR_PATH_PLACEHOLDER # Control freshness with phfc
+grive -u -s $STR_DIR_GOOGLE_DRIVE_TARGET -p $STR_PATH_SYNCDIR # upload changes by grive2
