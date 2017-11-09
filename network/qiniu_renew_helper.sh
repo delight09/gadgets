ee50394c348fe1de96c3752cda5a281aca96c58a
Jiahao Dai <hacka@djh.im>
2017-11-09 10:57:27 +0800

    Silence curl progress output

--- network/qiniu_renew_helper.sh
+++ network/qiniu_renew_helper.sh
@@ -11,6 +11,6 @@ single_url=$1
 TK=$(echo "/v2/tune/refresh" |openssl dgst -binary -hmac $SECRET_SK -sha1 |base64 | tr + - | tr / _)
 
 _curl_data="{\"urls\":[\" $single_url \"]}"
-_curl_cmd="curl -X POST -H '""Authorization: QBox ${SECRET_AK}:${TK}""' http://fusion.qiniuapi.com/v2/tune/refresh -d '"$_curl_data"' -H Content-Type:application/json"
+_curl_cmd="curl -sSL -X POST -H '""Authorization: QBox ${SECRET_AK}:${TK}""' http://fusion.qiniuapi.com/v2/tune/refresh -d '"$_curl_data"' -H Content-Type:application/json"
 
 eval "$_curl_cmd"
