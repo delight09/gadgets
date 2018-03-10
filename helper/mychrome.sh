78dfebdee1f1df53a0c3ddaae1151e1dbc14d1d3
Jiahao Dai <hacka@djh.im>
2018-03-10 09:32:40 +0800

    Add helper script for hosts w/ better chrome bootstrap script

--- helper/mychrome.sh
+++ helper/mychrome.sh
@@ -1,4 +1,5 @@
 #!/bin/sh --
-# Run inox with pac from fs
+# Run inox with preference
 
-inox --proxy-pac-url=file:///var/cache/pacfile/my.pac
+## inox --proxy-pac-url=file:///var/cache/pacfile/my.pac
+inox --proxy-server=http://doorkeeper:3128 --proxy-bypass-list="*.djh.im;localhost"
